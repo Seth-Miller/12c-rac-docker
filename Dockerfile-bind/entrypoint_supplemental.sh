2faa75b8b6052f015d9b0b7cd71c29727bde36cd
Seth-Miller <sethmiller.sm@gmail.com>
2016-10-12 18:47:07 +0000

    cleaned up

--- Dockerfile-bind/entrypoint_supplemental.sh
+++ Dockerfile-bind/entrypoint_supplemental.sh
@@ -4,14 +4,17 @@ set -e
 
 KEYS_DATA_DIR=${KEYS_DIR?}
 
-create_bind_data_dir() {
-  mkdir -p ${KEYS_DATA_DIR}
-  chmod -R 0775 ${KEYS_DATA_DIR}
-  chown -R ${BIND_USER}:${BIND_USER} ${KEYS_DATA_DIR}
+create_bind_key_dir() {
+  mkdir -p ${KEYS_DATA_DIR?}
+  chmod -R 0775 ${KEYS_DATA_DIR?}
+  chown -R ${BIND_USER}:${BIND_USER} ${KEYS_DATA_DIR?}
 
   # move keys into new keys directory
-  if [ ! -d ${KEYS_DATA_DIR}/keys ]; then
-    mv /etc/keys ${KEYS_DATA_DIR}/keys
+  if [ ! -d ${KEYS_DATA_DIR?}/keys ]; then
+    mv /etc/keys ${KEYS_DATA_DIR?}/keys
   fi
   rm -rf /etc/keys
 }
+
+
+create_bind_key_dir
