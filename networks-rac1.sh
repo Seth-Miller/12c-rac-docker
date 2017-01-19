0e5ef6a6eb9e18d96904b33928b139cc4a3227ba
Seth Miller <smiller.sm@gmail.com>
2017-01-19 22:25:34 +0000

    Update rac node network scripts Added changes that also start up dhclient systemd services.

--- networks-rac1.sh
+++ networks-rac1.sh
@@ -1,31 +1,63 @@
 #!/bin/bash
 
 
-NSPID=$(docker inspect --format='{{ .State.Pid }}' rac1)
+NSPID=$(/usr/bin/docker inspect --format='{{ .State.Pid }}' rac1)
 
-rm -rf "/var/run/netns/${NSPID?}"
-mkdir -p "/var/run/netns"
-ln -s "/proc/${NSPID?}/ns/net" "/var/run/netns/${NSPID?}"
+/usr/bin/rm -rf "/var/run/netns/${NSPID?}"
+/usr/bin/mkdir -p "/var/run/netns"
+/usr/bin/ln -s "/proc/${NSPID?}/ns/net" "/var/run/netns/${NSPID?}"
+
+BRIDGE=$(/usr/bin/docker network ls -q -f NAME=pub)
+/usr/bin/ip link del dev rac1-pub 2>/dev/null
+/usr/bin/ip link add name rac1-pub mtu 1500 type veth peer name eth-pub mtu 1500
+/usr/bin/sleep 5
+/usr/bin/ip link set rac1-pub master br-${BRIDGE?}
+/usr/bin/ip link set rac1-pub up
+/usr/bin/ip link set eth-pub netns ${NSPID?}
+/usr/bin/ip netns exec ${NSPID?} /usr/bin/ip link set eth-pub up
+
+/usr/bin/docker exec rac1 \
+/usr/bin/rm -f /etc/systemd/system/dhclient-rac1-eth-pub.service
+
+/usr/bin/docker exec rac1 \
+/usr/bin/ln -s \
+/usr/lib/custom_services/dhclient-rac1-eth-pub.service \
+/etc/systemd/system/dhclient-rac1-eth-pub.service
+
+/usr/bin/docker exec rac1 \
+/usr/bin/systemctl stop dhclient-rac1-eth-pub.service
+
+/usr/bin/docker exec rac1 \
+/usr/bin/systemctl daemon-reload
+
+/usr/bin/docker exec rac1 \
+/usr/bin/systemctl start dhclient-rac1-eth-pub.service
+
+BRIDGE=$(/usr/bin/docker network ls -q -f NAME=priv)
+/usr/bin/ip link del dev rac1-priv 2>/dev/null
+/usr/bin/ip link add name rac1-priv mtu 1500 type veth peer name eth-priv mtu 1500
+/usr/bin/sleep 5
+/usr/bin/ip link set rac1-priv master br-${BRIDGE?}
+/usr/bin/ip link set rac1-priv up
+/usr/bin/ip link set eth-priv netns ${NSPID?}
+/usr/bin/ip netns exec ${NSPID?} /usr/bin/ip link set eth-priv up
+
+/usr/bin/docker exec rac1 \
+/usr/bin/rm -f /etc/systemd/system/dhclient-rac1-eth-priv.service
+
+/usr/bin/docker exec rac1 \
+/usr/bin/ln -s \
+/usr/lib/custom_services/dhclient-rac1-eth-priv.service \
+/etc/systemd/system/dhclient-rac1-eth-priv.service
+
+/usr/bin/docker exec rac1 \
+/usr/bin/systemctl stop dhclient-rac1-eth-priv.service
+
+/usr/bin/docker exec rac1 \
+/usr/bin/systemctl daemon-reload
+
+/usr/bin/docker exec rac1 \
+/usr/bin/systemctl start dhclient-rac1-eth-priv.service
 
 
-BRIDGE=$(docker network ls -q -f NAME=pub)
-
-ip link del dev rac1-pub 2>/dev/null
-ip link add name rac1-pub mtu 1500 type veth peer name eth-pub mtu 1500
-ip link set rac1-pub master br-${BRIDGE?}
-ip link set rac1-pub up
-ip link set eth-pub netns ${NSPID?}
-ip netns exec ${NSPID?} ip link set eth-pub up
-
-
-BRIDGE=$(docker network ls -q -f NAME=priv)
-
-ip link del dev rac1-priv 2>/dev/null
-ip link add name rac1-priv mtu 1500 type veth peer name eth-priv mtu 1500
-ip link set rac1-priv master br-${BRIDGE?}
-ip link set rac1-priv up
-ip link set eth-priv netns ${NSPID?}
-ip netns exec ${NSPID?} ip link set eth-priv up
-
-
-rm -rf "/var/run/netns/${NSPID?}"
+/usr/bin/rm -rf "/var/run/netns/${NSPID?}"
--- networks-rac2.sh
+++ networks-rac2.sh
@@ -1,31 +1,63 @@
 #!/bin/bash
 
 
-NSPID=$(docker inspect --format='{{ .State.Pid }}' rac2)
+NSPID=$(/usr/bin/docker inspect --format='{{ .State.Pid }}' rac2)
 
-rm -rf "/var/run/netns/${NSPID?}"
-mkdir -p "/var/run/netns"
-ln -s "/proc/${NSPID?}/ns/net" "/var/run/netns/${NSPID?}"
+/usr/bin/rm -rf "/var/run/netns/${NSPID?}"
+/usr/bin/mkdir -p "/var/run/netns"
+/usr/bin/ln -s "/proc/${NSPID?}/ns/net" "/var/run/netns/${NSPID?}"
+
+BRIDGE=$(/usr/bin/docker network ls -q -f NAME=pub)
+/usr/bin/ip link del dev rac2-pub 2>/dev/null
+/usr/bin/ip link add name rac2-pub mtu 1500 type veth peer name eth-pub mtu 1500
+/usr/bin/sleep 5
+/usr/bin/ip link set rac2-pub master br-${BRIDGE?}
+/usr/bin/ip link set rac2-pub up
+/usr/bin/ip link set eth-pub netns ${NSPID?}
+/usr/bin/ip netns exec ${NSPID?} /usr/bin/ip link set eth-pub up
+
+/usr/bin/docker exec rac2 \
+/usr/bin/rm -f /etc/systemd/system/dhclient-rac2-eth-pub.service
+
+/usr/bin/docker exec rac2 \
+/usr/bin/ln -s \
+/usr/lib/custom_services/dhclient-rac2-eth-pub.service \
+/etc/systemd/system/dhclient-rac2-eth-pub.service
+
+/usr/bin/docker exec rac2 \
+/usr/bin/systemctl stop dhclient-rac2-eth-pub.service
+
+/usr/bin/docker exec rac2 \
+/usr/bin/systemctl daemon-reload
+
+/usr/bin/docker exec rac2 \
+/usr/bin/systemctl start dhclient-rac2-eth-pub.service
+
+BRIDGE=$(/usr/bin/docker network ls -q -f NAME=priv)
+/usr/bin/ip link del dev rac2-priv 2>/dev/null
+/usr/bin/ip link add name rac2-priv mtu 1500 type veth peer name eth-priv mtu 1500
+/usr/bin/sleep 5
+/usr/bin/ip link set rac2-priv master br-${BRIDGE?}
+/usr/bin/ip link set rac2-priv up
+/usr/bin/ip link set eth-priv netns ${NSPID?}
+/usr/bin/ip netns exec ${NSPID?} /usr/bin/ip link set eth-priv up
+
+/usr/bin/docker exec rac2 \
+/usr/bin/rm -f /etc/systemd/system/dhclient-rac2-eth-priv.service
+
+/usr/bin/docker exec rac2 \
+/usr/bin/ln -s \
+/usr/lib/custom_services/dhclient-rac2-eth-priv.service \
+/etc/systemd/system/dhclient-rac2-eth-priv.service
+
+/usr/bin/docker exec rac2 \
+/usr/bin/systemctl stop dhclient-rac2-eth-priv.service
+
+/usr/bin/docker exec rac2 \
+/usr/bin/systemctl daemon-reload
+
+/usr/bin/docker exec rac2 \
+/usr/bin/systemctl start dhclient-rac2-eth-priv.service
 
 
-BRIDGE=$(docker network ls -q -f NAME=pub)
-
-ip link del dev rac2-pub 2>/dev/null
-ip link add name rac2-pub mtu 1500 type veth peer name eth-pub mtu 1500
-ip link set rac2-pub master br-${BRIDGE?}
-ip link set rac2-pub up
-ip link set eth-pub netns ${NSPID?}
-ip netns exec ${NSPID?} ip link set eth-pub up
-
-
-BRIDGE=$(docker network ls -q -f NAME=priv)
-
-ip link del dev rac2-priv 2>/dev/null
-ip link add name rac2-priv mtu 1500 type veth peer name eth-priv mtu 1500
-ip link set rac2-priv master br-${BRIDGE?}
-ip link set rac2-priv up
-ip link set eth-priv netns ${NSPID?}
-ip netns exec ${NSPID?} ip link set eth-priv up
-
-
-rm -rf "/var/run/netns/${NSPID?}"
+/usr/bin/rm -rf "/var/run/netns/${NSPID?}"
